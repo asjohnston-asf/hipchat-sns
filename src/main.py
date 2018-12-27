30aeaf2ba7852d7c448c5cea324b837fb298be6f
Andrew Johnston <asjohnston@alaska.edu>
2018-12-27 00:11:54 +0000

    add additional parameters to the hipchat notification api call

--- src/main.py
+++ src/main.py
@@ -17,6 +17,10 @@ def lambda_handler(event, context):
     log.info(event)
     for record in event['Records']:
         data = {
+          'notify': 'true',
+          'color': 'red',
+          'message_format': 'text',
+          'from': record['Sns']['TopicArn'].split(':')[-1],
           'message': record['Sns']['Message'],
         }
         session.post(getenv('HIPCHAT_ENDPOINT'), data=json.dumps(data))
